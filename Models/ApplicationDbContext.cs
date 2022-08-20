5aaf83d7e297f1c17e4548375ae8222b1ce39c87
Ogbonna Ikenna Sydney <cindionline@yahoo.com>
2022-08-20 14:48:21 +0100

    Project Setup, Create and Edit Features

---
+++ Models/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace expenseTracker.Models
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<expenseTracker.Transaction> Transactions{ get; set; }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
