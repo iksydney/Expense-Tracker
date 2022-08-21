1a3a4fa22737f5ad5f76a9fa5a70d63db007974d
Ogbonna Ikenna Sydney <cindionline@yahoo.com>
2022-08-21 14:48:52 +0100

    Delete feature

--- Controllers/CategoryController.cs
+++ Controllers/CategoryController.cs
@@ -76,25 +76,6 @@ namespace expenseTracker.Controllers
             return View(category);
         }
 
-
-        // GET: Category/Delete/5
-        public async Task<IActionResult> Delete(int? id)
-        {
-            if (id == null || _context.Categories == null)
-            {
-                return NotFound();
-            }
-
-            var category = await _context.Categories
-                .FirstOrDefaultAsync(m => m.CategoryId == id);
-            if (category == null)
-            {
-                return NotFound();
-            }
-
-            return View(category);
-        }
-
         // POST: Category/Delete/5
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
@@ -112,11 +93,6 @@ namespace expenseTracker.Controllers
             
             await _context.SaveChangesAsync();
             return RedirectToAction(nameof(Index));
-        }
-
-        private bool CategoryExists(int id)
-        {
-          return (_context.Categories?.Any(e => e.CategoryId == id)).GetValueOrDefault();
         }
     }
 }
--- Models/Category.cs
+++ Models/Category.cs
@@ -14,7 +14,8 @@ namespace expenseTracker.Models
         [Column(TypeName = "nvarchar(10)")]
         public string Type { get; set; } = "Expense";
         [NotMapped]
-        public string? TitleWithIcon { 
+        public string? TitleWithIcon
+        { 
             get 
             {
                 return this.Icon + " " + this.Title;
