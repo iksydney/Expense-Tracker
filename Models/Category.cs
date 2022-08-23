c5bad991d66efa70d2d10baaedddae5ae8f26c43
Ogbonna Ikenna Sydney <cindionline@yahoo.com>
2022-08-23 02:51:14 +0100

    Transactions features

--- Models/Category.cs
+++ Models/Category.cs
@@ -8,6 +8,7 @@ namespace expenseTracker.Models
         [Key]
         public int CategoryId { get; set; }
         [Column(TypeName = "nvarchar(50)")]
+        [Required(ErrorMessage = "Title is required")]
         public string Title { get; set; }
         [Column(TypeName = "nvarchar(5)")]
         public string Icon { get; set; } = "";
